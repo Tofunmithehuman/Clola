03b664a3a55cdada41938c330d8b917824987c36
Bolaji OLuwatofunmi <tofunmibolaji13@gmail.com>
2025-09-02 15:57:38 +0100

    update: home second section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -38,31 +38,29 @@ function Home() {
         </div>
       </section>
 
-      <section className="h-screen">
-        <div>
-          <div className="max-w-5xl m-auto mt-10 p-4">
-            <h1 className="text-center text-3xl md:text-4xl font-thin leading-12 mb-2 0">
-              Boutique is believe in providing a personalized shopping
-              experience and exceptional customer service, just like you would
-              expect from a physical boutique.
-            </h1>
+       <section className="min-h-screen py-16 lg:py-24">
+        <div className="max-w-5xl mx-auto px-4">
+          <h1 className="text-center text-3xl md:text-4xl font-thin leading-relaxed mb-16">
+            Boutique believes in providing a personalized shopping
+            experience and exceptional customer service, just like you would
+            expect from a physical boutique.
+          </h1>
 
+          <div className="flex flex-col lg:flex-row gap-8">
+            <img src={CosmeticOne} alt="Cosmetics" className="lg:w-1/2 object-cover" />
 
-            <div className="flex flex-col lg:flex-row  gap-8 mt-16">
-              <img src={CosmeticOne} alt="Cosmetics" className="lg:w-1/2" />
+            <div className="lg:w-1/2 flex flex-col gap-6 justify-between">
+              <img src={CosmeticTwo} alt="Cosmetics" className="object-cover" />
 
-              <div  className="lg:w-1/2 flex flex-col gap-4 justify-between" >
-                <img src={CosmeticTwo} alt="Cosmetics"/>
+              <p className="font-thin text-lg">
+                Discover our exclusive range of boutique beauty products and experience the transformative power of beauty with us.
+              </p>
 
-                <p className="font-thin text-lg">Discover our exclusive range of boutique beauty products and experience the transformative power of beauty with us.</p>
-
-                 <button className="border hover:bg-black hover:text-white duration-200 py-4 px-8 flex items-center justify-center w-full lg:w-auto gap-2">
-                  Explore Now <ArrowUpRight />
-                </button>
-              </div>
+              <button className="border hover:bg-black hover:text-white duration-200 py-4 px-8 flex items-center justify-center w-full lg:w-auto gap-2">
+                Explore Now <ArrowUpRight />
+              </button>
+            </div>
           </div>
-          </div>
-
         </div>
       </section>
 
